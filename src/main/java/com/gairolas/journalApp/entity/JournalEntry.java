b97c32193201ab110b099f508e51921fc5e3a436
Sidhant Gairola <ssid7074@gmail.com>
2025-06-21 15:51:01 +0530

    added lombok dependency so that getters and setters will be created automatically

--- src/main/java/com/gairolas/journalApp/entity/JournalEntry.java
+++ src/main/java/com/gairolas/journalApp/entity/JournalEntry.java
@@ -6,7 +6,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Document(collection = "journal_entries")
+@Getter
+@Setter
 public class JournalEntry {
 
     @Id
@@ -16,36 +21,36 @@ public class JournalEntry {
     private String content;
     private LocalDateTime date;
 
-    public ObjectId getId() {
-        return id;
-    }
+    // public ObjectId getId() {
+    //     return id;
+    // }
 
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
+    // public void setId(ObjectId id) {
+    //     this.id = id;
+    // }
 
-    public LocalDateTime getDate() {
-        return date;
-    }
+    // public LocalDateTime getDate() {
+    //     return date;
+    // }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
+    // public void setDate(LocalDateTime date) {
+    //     this.date = date;
+    // }
 
-    public String getTitle() {
-        return title;
-    }
+    // public String getTitle() {
+    //     return title;
+    // }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+    // public void setTitle(String title) {
+    //     this.title = title;
+    // }
 
-    public String getContent() {
-        return content;
-    }
+    // public String getContent() {
+    //     return content;
+    // }
 
-    public void setContent(String content) {
-        this.content = content;
-    }
+    // public void setContent(String content) {
+    //     this.content = content;
+    // }
 
 }
