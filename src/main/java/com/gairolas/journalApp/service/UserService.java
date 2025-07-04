466f2c4639d65253fb89a3dff442de20593c527d
Sidhant Gairola <ssid7074@gmail.com>
2025-07-05 01:55:17 +0530

    Logging added

--- src/main/java/com/gairolas/journalApp/service/UserService.java
+++ src/main/java/com/gairolas/journalApp/service/UserService.java
@@ -4,7 +4,11 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.bson.types.ObjectId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,16 +18,29 @@ import com.gairolas.journalApp.entity.User;
 import com.gairolas.journalApp.repository.UserRepository;
 
 @Component
+@Slf4j
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public void saveNewUser(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.setRoles(Arrays.asList("USER"));
-        userRepository.save(user);
+    public boolean saveNewUser(User user) {
+        try {
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setRoles(Arrays.asList("USER"));
+            userRepository.save(user);
+            return true;
+        } catch (Exception e) {
+            log.error("Error for : {}", user.getUserName(), e);
+/*
+            log.error("new log inserted");
+            log.warn("new log inserted");
+            log.debug("new log inserted");
+            log.trace("new log inserted");
+*/
+            return false;
+        }
     }
 
     public void saveUser(User user) {
