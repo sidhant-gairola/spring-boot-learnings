ced6bcdff03c6d976421c28b6e449d7eed0b9c28
Sidhant Gairola <ssid7074@gmail.com>
2025-07-15 01:35:51 +0530

    new field added as sentiment

--- src/main/java/com/gairolas/journalApp/entity/JournalEntry.java
+++ src/main/java/com/gairolas/journalApp/entity/JournalEntry.java
@@ -2,6 +2,7 @@ package com.gairolas.journalApp.entity;
 
 import java.time.LocalDateTime;
 
+import com.gairolas.journalApp.enums.Sentiment;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -24,37 +25,5 @@ public class JournalEntry {
     private String title;
     private String content;
     private LocalDateTime date;
-
-    // public ObjectId getId() {
-    //     return id;
-    // }
-
-    // public void setId(ObjectId id) {
-    //     this.id = id;
-    // }
-
-    // public LocalDateTime getDate() {
-    //     return date;
-    // }
-
-    // public void setDate(LocalDateTime date) {
-    //     this.date = date;
-    // }
-
-    // public String getTitle() {
-    //     return title;
-    // }
-
-    // public void setTitle(String title) {
-    //     this.title = title;
-    // }
-
-    // public String getContent() {
-    //     return content;
-    // }
-
-    // public void setContent(String content) {
-    //     this.content = content;
-    // }
-
+    private Sentiment sentiment;
 }
